35c4cce637b13f4973becc417dbee705132965a4
Mario Leone <mleone10@gmail.com>
2021-01-18 10:54:03 -0500

    Add files to start QuickBooks package

---
+++ quickbooks/client.go
@@ -0,0 +1,5 @@
+package quickbooks
+
+// A Client is an wrapper around other QuickBooks interactions which handles authentication, request construction, and response parsing.
+type Client struct {
+}
